af05b6574fe162432827af9ad8186c4a0bb9995f
Aaron Villanueva <aarone.villanueva@gmail.com>
2019-05-04 15:16:41 -0700

    figures out how to use esc key to dismiss toolip on focus

--- tooltip/index.js
+++ tooltip/index.js
@@ -1,39 +1,9 @@
 'use strict'
 //uses es5
 
-document.querySelector('button').addEventListener('click' Tooltip);
+document.querySelector('button').addEventListener('keydown', function(e) {
+  var tooltip = document.querySelector('[role=tooltip]');
+  if (e.key.match('Esc')) { tooltip.style.opacity = '0';}
+});
 
-function Tooltip(e) {
-  var el = e.target;
 
-  var tooltip = (function() {
-    var tltip = document.createElement('span');
-    tltip.setAttribute('role', 'tooltip');
-    tltip.innerText = 'testing'
-    return tltip;
-  })();
-
-  // to help hover from tigger to tooltip without dissappearing
-  var balloon = (function() { 
-    var balloon = document.createElement('div');
-    balloon.classList.add('tooltip');
-    balloon.appendChild('tooltip');
-  })();
-
-  function open() {
-    // delay open 4 seconds
-    setTimeout(function() {
-      el.parentNode.appendChild('balloon');
-    }, 4000);
-  };
-
-  function close() {
-    el.parentNode.removeChild('balloon');
-  };
-
-  el.addEventListener('focus', open);
-  el.addEventListener('mouseover', open);
-
-  el.addEventListener('blur', close);
-  el.addEventListener('mouseout', close);
-}
