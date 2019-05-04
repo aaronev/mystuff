41656486192c6147187c4b10b8db0ed49f95b328
Aaron Villanueva <aarone.villanueva@gmail.com>
2019-05-04 16:10:13 -0700

    figures out how to dismiss tooltip with esc on focus and on mouse

--- tooltip/index.js
+++ tooltip/index.js
@@ -1,9 +1,58 @@
 'use strict'
 //uses es5
 
-document.querySelector('button').addEventListener('keydown', function(e) {
-  var tooltip = document.querySelector('[role=tooltip]');
-  if (e.key.match('Esc')) { tooltip.style.opacity = '0';}
+
+
+
+// document.querySelector('[role=tooltip]').addEventListener('keydown', escDismissTooltip);
+
+Array.prototype.slice.call(document.querySelectorAll('button')).forEach(function(el){
+  var tooltip = el.nextElementSibling;
+
+  el.addEventListener('keydown', escDismissTooltip);
+  el.addEventListener('blur', resetTooltip);
+  el.addEventListener('mouseout', resetTooltip);
+  document.onmouseover = function(e) {
+    document.elementFromPoint(e.clientX, e.clientY).parentNode.addEventListener('keydown', escDismissTooltip)
+  };
+
+  //this will overwrite css
+  function escDismissTooltip(e) {
+    if (e.key.match('Esc')) { 
+      tooltip.style.opacity = '0';
+    }
+  };
+
+  //reset to css default
+  function resetTooltip() { 
+    tooltip.style.opacity = '';
+  };
+
 });
 
+// (function() { //track mouse 
+//     document.onmousemove = handleMouseMove;
+//     function handleMouseMove(event) {
+//         var eventDoc, doc, body;
 
+//         event = event || window.event; // IE-ism
+
+//         // If pageX/Y aren't available and clientX/Y are,
+//         // calculate pageX/Y - logic taken from jQuery.
+//         // (This is to support old IE)
+//         if (event.pageX == null && event.clientX != null) {
+//             eventDoc = (event.target && event.target.ownerDocument) || document;
+//             doc = eventDoc.documentElement;
+//             body = eventDoc.body;
+
+//             event.pageX = event.clientX +
+//               (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+//               (doc && doc.clientLeft || body && body.clientLeft || 0);
+//             event.pageY = event.clientY +
+//               (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+//               (doc && doc.clientTop  || body && body.clientTop  || 0 );
+//         }
+
+//         // Use event.pageX / event.pageY here
+//     }
+// })();
